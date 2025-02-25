821c8cffd57429b07db6eafc4c6ea5be784bb9dc
SKVLT\scottk <keelers@wclc.com>
2025-02-25 11:19:36 -0600

    init commit for CM app

---
+++ src/app-js.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChangeManagement from './components/ChangeManagement';
+
+function App() {
+  return (
+    <div className="App">
+      <ChangeManagement />
+    </div>
+  );
+}
+
+export default App;
